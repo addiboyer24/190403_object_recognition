c59c0a3566b3a46e333efc05738e29e1de58387a
Addison Wesley Boyer <addisonwesleyboyer@Addisons-MacBook-Air.local>
2019-04-18 23:56:12 -0600

    Adding cleaned up class activation mapping function

---
+++ cam.py
@@ -0,0 +1,8 @@
+def class_activation_mapping():
+       pass
+import numpy as np
+myList = [[[1,1,1],[1,1,1],[1,1,1]], [[2,2,2],[2,2,2],[2,2,2]]]
+myList = np.array(myList)
+
+for item in zip(myList):
+       print(item)
